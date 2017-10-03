1f351cc1b5be30a8db6a9b622a07838bd7dff914
Omair <omairraza9@gmail.com>
2017-10-03 18:41:47 -0400

    added the redirect

--- server.rb
+++ server.rb
@@ -15,6 +15,18 @@ end
 
 get '/about_me' do
   # instructions for what to do when user visits '/home' will go here
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+    @interests = ['cats', 'art', 'music', 'films', 'coffee']
+    erb :about_me
+end
 
-  erb(:about_me)
+get '/favourites' do
+#if you go to http://localhost:4567/portfolio my link should appear
+@fav_links =['https://www.youtube.com', 'https://www.Podcasts.com', 'https://www.coinbase.com']
+
+erb(:favourites)
+end
+
+get '/' do
+  redirect to('/portfolio')
 end
